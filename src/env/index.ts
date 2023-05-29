374e111c9ede67498e045077bab2233cd078973a
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-05-29 19:34:50 +0100

    feat: config eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,12 +3,12 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(envSchema)
 
-if(_env.success === false){
+if (_env.success === false) {
   const errorMessage = 'Invalid environment variables.'
   console.log(errorMessage, _env.error.format())
   throw new Error(errorMessage)
