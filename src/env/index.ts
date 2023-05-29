18e48b550cdd6343ea4e44e24804042436257c0a
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-05-29 19:25:15 +0100

    feat: valid env variables

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchema.safeParse(envSchema)
+
+if(_env.success === false){
+  const errorMessage = 'Invalid environment variables.'
+  console.log(errorMessage, _env.error.format())
+  throw new Error(errorMessage)
+}
+
+export const env = _env.data
