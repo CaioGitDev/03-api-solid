eaea9e07a87c9ee960066b85a6c7fd87f0ff45d3
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-05-31 22:38:46 +0100

    test: should not be able to authenticate with wrong email

--- src/services/authenticate.spec.ts
+++ src/services/authenticate.spec.ts
@@ -3,6 +3,7 @@ import { hash } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { fakerPT_PT as faker } from '@faker-js/faker'
 import { AuthenticateServer } from './authenticate'
+import { InvalidCredentialsError } from './erros/invalid-credentials-error'
 
 describe('Authenticate Service', () => {
   it('should be able to authenticate', async () => {
@@ -24,5 +25,20 @@ describe('Authenticate Service', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateServer(usersRepository)
+
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+
+    expect(() =>
+      sut.execute({
+        email,
+        password,
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
