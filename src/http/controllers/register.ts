a89d45175a354cfa49cffacbef9c30f6e4314c10
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-05-31 20:17:26 +0100

    feat: add error global handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -29,7 +29,8 @@ export const register = async (
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message })
     }
-    return reply.status(500).send() // TODO: fix me
+
+    throw error
   }
 
   return reply.status(201).send()
