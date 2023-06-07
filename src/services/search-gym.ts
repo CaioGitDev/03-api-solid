9be4aefffdf686128cde1eddbb972309ab156f0c
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-06-07 14:05:29 +0100

    feat(service): Add SearchGymServiceRequest functionality

---
+++ src/services/search-gym.ts
@@ -0,0 +1,25 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+
+interface SearchGymServiceRequest {
+  query: string
+  page: number
+}
+interface SearchGymServiceResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymService {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymServiceRequest): Promise<SearchGymServiceResponse | null> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    return {
+      gyms,
+    }
+  }
+}
